0cbf840ac2d41529496d7c73fbc87809f34d0964
Mark Adler <mark.adler@pcc.edu>
2019-10-21 23:10:05 -0700

    added print feature

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -2,7 +2,18 @@ package edu.pcc.cis.helloworld;
 
 /**
  * Hello World
+ * 2019.10.21 Version
+ * Created by Mark Adler
  */
 
 public class HelloWorld {
+
+    /**
+     * Prints 'Hello, World!' to console.
+     */
+
+    public void sayHello() {
+        System.out.println("Hello, World!");
+    }
+
 }
